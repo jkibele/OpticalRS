07c35d5680b49b1cb3d56f12ea1cb7d579ff39c7
jkibele <jkibele@gmail.com>
2014-07-23 18:41:58 +1200

    I dunno what I did but there's a change so I'll commit it.

--- OpticalRS/Lyzenga1981.py
+++ OpticalRS/Lyzenga1981.py
@@ -16,6 +16,11 @@ import numpy as np
 from math import sqrt
 from fractions import Fraction
 from decimal import Decimal
+from itertools import combinations
+
+##-- Straight from the paper --##
+## The following 2 methods are meant to implement methods explicitly described
+## in Lyzenga 1981.
 
 def attenuation_coef_ratio(band_i,band_j,transform=np.log):
     r"""
@@ -83,3 +88,60 @@ def di_index(imarr,sandarr,i,j):
     Bj = imarr[:,:,j]
     DI = ( Kj * np.log(Bi) - Ki * np.log(Bj) ) / sqrt( Ki**2 + Kj**2 )
     return DI
+    
+##-- Implied by Lyzenga 1981 --##
+## The following method simply applies Lyzenga's methods to a mulitspectral
+## image. This may be more convenient that manually running di_index for 
+## each band combination.
+    
+def di_indexes_bandarr(imarr,sandarr,n_bands,subset_slice=None,pix_band_shaped=False):
+    """
+    Generate a depth invariant index image for each possible band combination
+    and output as a multispectral array of shape (Rows,Columns,Combos). Also 
+    return a list integer tuples of band combinations.
+    
+    Args:
+        imarr (numpy.array): This is the image array of shape (Rows,Cols,Bands)
+        
+        sandarr (numpy.array): This is an array image array that contains only
+            pixels from a uniform bottom type (usually sand) and varying depth.
+            Values from this array will be passed to the attenuation_coef_ratio
+            method.
+        
+        n_bands (int): The number of bands you want to generate indexes for.
+            There generally doesn't seem to be much point in including the NIR
+            bands.
+            
+        subset_slice (numpy.s_ index expression): If provided, only this subset
+            of the array will be used to calculate the indexes. This is useful
+            for testing things out if processing is slow. For example, 
+            np.s_[1300:1800,1200:2400] would subset image rows 1300 - 1800 and
+            columns 1200 - 2400.
+            
+        pix_band_shaped (bool): If true, indexes will be flattened. Rather than
+            returning (Rows,Columns,N Combos) shape, (N Pixels, N Combos) shape
+            will be returned.
+            
+    Returns:
+        di_index_array (numpy.array): An array of depth invariant indexes. The
+            last dimension of the array corresponds to which combination of bands 
+            created that index. For instance (assuming pix_band_shaped=False),
+            di_index_array[:,:,x] was created by combos[x].
+            
+        combos (list of tuples): A list of tuples of integers. The integers are
+            zero indexed band numbers. If combos[x] = (0,1), then 
+            di_index_array[:,:,x] was created with imarr band 0 and band 1.
+    """
+    if subset_slice:
+        imarr = imarr[subset_slice]
+    combos = [cb for cb in combinations( range(n_bands), 2 ) ]
+    n_combos = len( combos )
+    arrdtype = imarr.dtype
+    out_arr = np.empty((imarr.shape[0],imarr.shape[1],n_combos),dtype=arrdtype)
+    for ind,bc in enumerate( combos ):
+        i,j = bc
+        di_ind = di_index(imarr,sandarr,i,j)
+        out_arr[:,:,ind] = di_ind
+    if pix_band_shaped:
+        out_arr = out_arr.reshape( out_arr.shape[0]*out_arr.shape[1], -1 )
+    return out_arr, combos
--- OpticalRS/__init__.py
+++ OpticalRS/__init__.py
@@ -2,6 +2,8 @@
 from OpticalRS import *
 from RasterDS import *
 import Lyzenga1981
+import Sagawa2010
+import LandMasking
 
 __author__ = 'Jared Kibele'
 __email__ = 'jkibele@gmail.com'
