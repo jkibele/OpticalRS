Added RasterSubset module. Also fixed Sagawa2010. It was giving incorrect results before. ...and I think I did some other stuff as well.

OpticalRS/MSDisplay.py
@@ -41,20 +41,16 @@ def multiband_histogram( img, nbins=256, figwidth=14 ):
         
 def view_bands( img, **kwargs ):
     nbands = img.shape[-1]
-    if kwargs.has_key('ncols'):
-        ncols = kwargs['ncols']
-    else:
-        ncols = 2
-    if kwargs.has_key('figwidth'):
-        figwidth = kwargs['figwidth']
-    else:
-        figwidth = 14
+    ncols = kwargs.pop('ncols',2)
+    figwidth = kwargs.pop('figwidth',14)
+    cmap = kwargs.pop('cmap',None)
     nrows = int( ceil( nbands/2.0 ))
+    subset = kwargs.pop('subset',np.s_[:,:,:])
     fig, axarr = subplots(nrows, ncols, sharex=True, sharey=True, figsize=(figwidth, (figwidth/3.5)*nrows))
     for i, barr in enumerate( np.dsplit(img, nbands) ):
         ax = axarr.ravel()[i]
         axtit = "Band %i" % (i + 1)
         ax.set_title(axtit)
-        ax.imshow( barr.squeeze() )
+        ax.imshow( barr[subset].squeeze(), cmap=cmap )
         ax.axis('off')
     

OpticalRS/RasterDS.py
@@ -9,7 +9,7 @@ Created on Fri Jun 27 14:58:13 2014
 """
 
 import os
-from osgeo import gdal, ogr
+from osgeo import gdal, ogr, osr
 from osgeo.gdalconst import *
 import numpy as np
 
@@ -63,6 +63,11 @@ class RasterDS(object):
         return self.gdal_ds.GetProjection()
         
     @property
+    def epsg(self):
+        srs = osr.SpatialReference(wkt=self.projection_wkt)
+        return int( srs.GetAttrValue('AUTHORITY',1) )
+        
+    @property
     def output_file_path(self):
         """
         Return a file path for output. Assume that we'll output same file extension.
@@ -89,14 +94,22 @@ class RasterDS(object):
         Return the image data in a numpy array of shape (Rows, Columns, Bands).
         If the image has a "no data value" return a masked array.
         """
-        barr = self.gdal_ds.ReadAsArray()
+        return self.band_array_subset()
+        
+    def band_array_subset(self,xoff=0, yoff=0, win_xsize=None, win_ysize=None):
+        """
+        Return the image data in a numpy array of shape (Rows, Columns, Bands).
+        If the image has a "no data value" return a masked array. Take a subset
+        if subset values are given.
+        """
+        #barr = self.gdal_ds.ReadAsArray()
         #ourarr = barr.T.swapaxes(0,1)
         blist = []
         bmlist = []
         for b in range( self.gdal_ds.RasterCount ):
             b += 1 #Bands are 1 based indexed
             band = self.gdal_ds.GetRasterBand( b )
-            barr = band.ReadAsArray()
+            barr = band.ReadAsArray(xoff=xoff,yoff=yoff,win_xsize=win_xsize,win_ysize=win_ysize)
             blist.append( barr )
             nodat = band.GetNoDataValue()
             if nodat is not None:
@@ -107,6 +120,9 @@ class RasterDS(object):
         allbands.mask = np.dstack( bmlist )
         if nodat is not None:
             allbands.set_fill_value( nodat )
+        # make sure that a single band raster will return (Rows,Columns,1(Band))
+        if allbands.ndim==2:
+            np.expand_dims(allbands,2)
         return allbands
         
     def new_image_from_array(self,bandarr,outfilename=None,dtype=GDT_Float32,no_data_value=-99):

OpticalRS/Sagawa2010.py
@@ -62,7 +62,8 @@ def band_attenuation_geometric(bandarr,deptharr):
     Given a single (Rows x Columns) band array and a depth array from the same 
     region, return -K*g value for that band. See section 2.2 and 2.3 from 
     Sagawa et al., 2010. The arrays you pass in should be taken from an area 
-    with uniform substrate and varying depth.
+    with uniform substrate and varying depth. The arrays must also be the same
+    shape and, if masked, the masks must be the same.
     
     Args:
         bandarr (numpy.array): Array of reflectance (or radiance or DN) values
@@ -87,9 +88,17 @@ def band_attenuation_geometric(bandarr,deptharr):
             reflectance index calculations but can give you an idea of how well
             the regression fit the data.        
     """
-    lnY = np.log(bandarr).flatten()
-    X = deptharr.flatten()
-    slope, intercept, r_value, p_value, std_err = linregress(X,lnY)
+    if np.ma.is_masked(deptharr):
+        X = deptharr.compressed()
+    else:
+        X = deptharr.ravel()
+        
+    if np.ma.is_masked(bandarr):
+        Y = bandarr.compressed()
+    else:
+        Y = bandarr.ravel()
+        
+    slope, intercept, r_value, p_value, std_err = linregress(X,np.log(Y))
     return slope, intercept, r_value
     
 def single_band_reflectance_index(single_band_arr,depth_arr,negKG):
@@ -117,7 +126,7 @@ def single_band_reflectance_index(single_band_arr,depth_arr,negKG):
             containing reflectance index values. This index is (or should be)
             linearly related to bottom reflectance.
     """
-    RI = single_band_arr / np.exp(negKG*depth_arr)
+    RI = np.log( single_band_arr ) - negKG * depth_arr
     return RI
     
 def negKg_regression_array(bandarr,deptharr,band_list=None):
@@ -223,4 +232,4 @@ def reflectance_index(bandarr,deptharr,negKgarr,band_list=None):
     for i in band_list:
         RI = single_band_reflectance_index(bandarr[:,:,i], deptharr, negKgarr[i])
         arrlist.append(RI)
-    return np.dstack(arrlist)
+    return np.ma.dstack(arrlist)