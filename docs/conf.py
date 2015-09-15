43ab97b6ecf40931a054fd33345ee6b20f6db1b0
jkibele <jkibele@gmail.com>
2015-09-15 14:23:16 +1200

    More messing with requirements to get this to build on readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['GDAL','matplotlib','geopandas','rasterstats','scipy','osgeo']
+MOCK_MODULES = ['GDAL','matplotlib','geopandas','rasterstats','scipy','osgeo',
+                'osgeo.gdalconst']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another
